59bd2fc3970d5a3b9e08fc19ead237be023428dc
setouchi-h <setouchi.kazuki@gmail.com>
2026-01-11 00:04:34 +0900

    feat(config): add system clipboard integration with macOS keymaps
    Enable clipboard synchronization with system clipboard (unnamedplus)
    and add Cmd+C/Cmd+V keymaps for macOS-native copy/paste behavior.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Cmd+C でコピー（ビジュアルモード用）
+vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy to clipboard" })
+-- Cmd+V でペースト
+vim.keymap.set({ "i", "c" }, "<D-v>", "<C-r>+", { desc = "Paste from clipboard" })
+vim.keymap.set("n", "<D-v>", '"+p', { desc = "Paste from clipboard" })
