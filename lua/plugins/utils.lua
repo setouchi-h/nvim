9184d419fce2357a3cb61b8e60877652b471df99
setouchi-h <setouchi.kazuki@gmail.com>
2026-01-11 13:54:34 +0900

    feat(plugins): add utils plugin with developer tools config
    Add utility plugin configuration including:
    - Telescope keymap for finding plugin files (<leader>fp)
    - Mason auto-installation of stylua, shellcheck, shfmt
    - Treesitter bash parser installation

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,37 @@
+return {
+  -- プラグインファイル検索キーマップ
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+  },
+
+  -- ツールの自動インストール
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+      })
+    end,
+  },
+
+  -- Treesitter追加パーサー
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "bash" })
+      end
+    end,
+  },
+}
