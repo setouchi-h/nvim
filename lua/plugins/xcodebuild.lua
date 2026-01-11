3f5407dfb6bc5e72996a7f7f3ee2f8aa131bc70f
setouchi-h <setouchi.kazuki@gmail.com>
2026-01-11 18:14:54 +0900

    refactor(xcodebuild): disable conflicting LazyVim keymaps
    Disable <leader>xt, <leader>xT from todo-comments.nvim and <leader>xx
    from trouble.nvim to prevent keymap conflicts with xcodebuild keymaps.
    Restructure plugin config to use proper multi-plugin lazy.nvim format.

--- lua/plugins/xcodebuild.lua
+++ lua/plugins/xcodebuild.lua
@@ -1,48 +1,61 @@
 return {
-  "wojciech-kulik/xcodebuild.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    -- デバッグ用
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui",
+  -- Disable conflicting LazyVim keymaps for xcodebuild
+  {
+    "folke/todo-comments.nvim",
+    keys = {
+      { "<leader>xt", false },
+      { "<leader>xT", false },
+    },
   },
-  config = function()
-    require("xcodebuild").setup({
-      -- ログパネルの設定
-      logs = {
-        auto_open_on_success_build = false,
-        auto_open_on_failed_build = true,
-        auto_focus = false,
-      },
-      -- コンソール出力（print文）
-      console = {
-        enabled = true,
-      },
-      -- コードカバレッジ
-      code_coverage = {
-        enabled = true,
-      },
-    })
+  {
+    "folke/trouble.nvim",
+    keys = {
+      { "<leader>xx", false },
+    },
+  },
+  -- xcodebuild.nvim
+  {
+    "wojciech-kulik/xcodebuild.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      -- デバッグ用
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      require("xcodebuild").setup({
+        -- ログパネルの設定
+        logs = {
+          auto_open_on_success_build = false,
+          auto_open_on_failed_build = true,
+          auto_focus = false,
+        },
+        -- コードカバレッジ
+        code_coverage = {
+          enabled = true,
+        },
+      })
 
-    -- キーマップ
-    local keymap = vim.keymap.set
-    keymap("n", "<leader>xS", "<cmd>XcodebuildSetup<cr>", { desc = "Setup Project" })
-    keymap("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build" })
-    keymap("n", "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build for Testing" })
-    keymap("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run" })
-    keymap("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
-    keymap("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run Test Class" })
-    keymap("n", "<leader>x.", "<cmd>XcodebuildTestRepeat<cr>", { desc = "Repeat Last Test" })
-    keymap("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Logs" })
-    keymap("n", "<leader>xo", "<cmd>XcodebuildToggleConsole<cr>", { desc = "Toggle Console (print)" })
-    keymap("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
-    keymap("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
-    keymap("n", "<leader>xs", "<cmd>XcodebuildSelectScheme<cr>", { desc = "Select Scheme" })
-    keymap("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
-    keymap("n", "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", { desc = "Coverage Report" })
-    keymap("n", "<leader>xq", "<cmd>Telescope quickfix<cr>", { desc = "Quickfix List" })
-    keymap("n", "<leader>xx", "<cmd>XcodebuildPicker<cr>", { desc = "Xcodebuild Commands" })
-  end,
+      -- キーマップ
+      local keymap = vim.keymap.set
+      keymap("n", "<leader>xS", "<cmd>XcodebuildSetup<cr>", { desc = "Setup Project" })
+      keymap("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build" })
+      keymap("n", "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build for Testing" })
+      keymap("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run" })
+      keymap("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
+      keymap("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run Test Class" })
+      keymap("n", "<leader>x.", "<cmd>XcodebuildTestRepeat<cr>", { desc = "Repeat Last Test" })
+      keymap("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Logs" })
+      keymap("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
+      keymap("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
+      keymap("n", "<leader>xs", "<cmd>XcodebuildSelectScheme<cr>", { desc = "Select Scheme" })
+      keymap("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
+      keymap("n", "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", { desc = "Coverage Report" })
+      keymap("n", "<leader>xq", "<cmd>Telescope quickfix<cr>", { desc = "Quickfix List" })
+      keymap("n", "<leader>xx", "<cmd>XcodebuildPicker<cr>", { desc = "Xcodebuild Commands" })
+      keymap("n", "<leader>xo", "<cmd>XcodebuildToggleConsole<cr>", { desc = "Toggle Console (print)" })
+    end,
+  },
 }
