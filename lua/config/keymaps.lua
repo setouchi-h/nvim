c25aa2d1cedb01b2997b57596bc064040994370b
setouchi-h <setouchi.kazuki@gmail.com>
2026-01-13 22:33:43 +0900

    feat(keymaps): add VSCode-style line move shortcuts
    Add Alt+Up/Down keymaps to move lines up/down in normal and visual mode.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,9 @@ vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy to clipboard" })
 -- Cmd+V でペースト
 vim.keymap.set({ "i", "c" }, "<D-v>", "<C-r>+", { desc = "Paste from clipboard" })
 vim.keymap.set("n", "<D-v>", '"+p', { desc = "Paste from clipboard" })
+
+-- 行を上下に移動（VSCode風）
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
